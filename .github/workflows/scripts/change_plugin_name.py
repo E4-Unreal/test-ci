737c710a0b4a41c4a9c057ba3bf47b37c801bdf7
이정훈 <eu4ng97@gmail.com>
2025-05-08 04:45:53 +0900

    Fix: 오류 수정

--- .github/workflows/scripts/change_plugin_name.py
+++ .github/workflows/scripts/change_plugin_name.py
@@ -5,4 +5,4 @@ from modules import unreal_plugin
 if __name__ == '__main__':
     old_plugin_name = os.environ['OLD_PLUGIN_NAME']
     new_plugin_name = os.environ['NEW_PLUGIN_NAME']
-    unreal_plugin.change_plugin_name(os.path.abspath('../../../'), old_plugin_name, new_plugin_name)
+    unreal_plugin.change_plugin_name(os.getcwd(), old_plugin_name, new_plugin_name)
