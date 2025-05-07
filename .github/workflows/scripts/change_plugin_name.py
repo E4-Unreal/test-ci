bde9867658047883c58c289504c4b75aa949b231
이정훈 <eu4ng97@gmail.com>
2025-05-07 17:06:03 +0000

    Feat: 플러그인 파일 수정 기능 추가

---
+++ .github/workflows/scripts/change_plugin_name.py
@@ -0,0 +1,12 @@
+import os
+
+old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
+new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
+
+plugin_file_path = old_plugin_name + '.uplugin'
+
+with open(plugin_file_path, 'r+') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.replace(old_plugin_name, new_plugin_name)
+        f.write(line)
--- .github/workflows/scripts/test.py
+++
@@ -1,7 +0,0 @@
-import os
-
-old_plugin_name = os.environ.get("OLD_PLUGIN_NAME")
-new_plugin_name = os.environ.get("NEW_PLUGIN_NAME")
-
-print(f'Old Plugin Name: {old_plugin_name}')
-print(f'New Plugin Name: {new_plugin_name}')
