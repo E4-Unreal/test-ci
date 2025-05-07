801159865bc0f3f7e2fbfc67bd51f8385cced3cf
이정훈 <eu4ng97@gmail.com>
2025-05-07 17:10:04 +0000

    Refactor: 함수 추출

--- .github/workflows/scripts/change_plugin_name.py
+++ .github/workflows/scripts/change_plugin_name.py
@@ -1,12 +1,16 @@
 import os
 
-old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
-new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
+def change_plugin_name():
+    old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
+    new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
 
-plugin_file_path = old_plugin_name + '.uplugin'
+    plugin_file_path = old_plugin_name + '.uplugin'
 
-with open(plugin_file_path, 'r+') as f:
-    lines = f.readlines()
+    with open(plugin_file_path, 'r+') as f:
+        lines = f.readlines()
     for line in lines:
         line = line.replace(old_plugin_name, new_plugin_name)
         f.write(line)
+        
+if __name__ == '__main__':
+    change_plugin_name()
