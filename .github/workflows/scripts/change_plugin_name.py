8698363519b46bc0be9e78092cd2cd7200c0aa7a
이정훈 <eu4ng97@gmail.com>
2025-05-07 17:30:59 +0000

    Feat: 커밋 푸시 기능 추가

--- .github/workflows/scripts/change_plugin_name.py
+++ .github/workflows/scripts/change_plugin_name.py
@@ -1,9 +1,15 @@
 import os
+import sys
 
-def change_plugin_name():
-    old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
-    new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
+def check_plugin_name(old_plugin_name, new_plugin_name):
+    if old_plugin_name is None or new_plugin_name is None
+        sys.exit('Plugin name is None')
+    elif old_plugin_name == new_plugin_name
+        sys.exit('Plugin name is same')
 
+def change_plugin_name(old_plugin_name, new_plugin_name):
+    check_plugin_name(old_plugin_name, new_plugin_name)
+    
     plugin_file_path = old_plugin_name + '.uplugin'
 
     with open(plugin_file_path, 'r+') as f:
@@ -13,4 +19,7 @@ def change_plugin_name():
         f.write(line)
         
 if __name__ == '__main__':
-    change_plugin_name()
+    old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
+    new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
+
+    change_plugin_name(old_plugin_name, new_plugin_name)
