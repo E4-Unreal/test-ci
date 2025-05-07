a2e08450bafb19b1f4a658efafd9b79c9882670f
이정훈 <eu4ng97@gmail.com>
2025-05-07 17:33:25 +0000

    Fix: 문법 수정

--- .github/workflows/scripts/change_plugin_name.py
+++ .github/workflows/scripts/change_plugin_name.py
@@ -2,14 +2,16 @@ import os
 import sys
 
 def check_plugin_name(old_plugin_name, new_plugin_name):
-    if old_plugin_name is None or new_plugin_name is None
+    if old_plugin_name is None or new_plugin_name is None:
         sys.exit('Plugin name is None')
-    elif old_plugin_name == new_plugin_name
+    elif old_plugin_name == new_plugin_name:
         sys.exit('Plugin name is same')
 
-def change_plugin_name(old_plugin_name, new_plugin_name):
+def change_plugin_name():
+    old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
+    new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
     check_plugin_name(old_plugin_name, new_plugin_name)
-    
+
     plugin_file_path = old_plugin_name + '.uplugin'
 
     with open(plugin_file_path, 'r+') as f:
@@ -17,9 +19,6 @@ def change_plugin_name(old_plugin_name, new_plugin_name):
     for line in lines:
         line = line.replace(old_plugin_name, new_plugin_name)
         f.write(line)
-        
-if __name__ == '__main__':
-    old_plugin_name = os.environ.get('OLD_PLUGIN_NAME')
-    new_plugin_name = os.environ.get('NEW_PLUGIN_NAME')
 
-    change_plugin_name(old_plugin_name, new_plugin_name)
+if __name__ == '__main__':
+    change_plugin_name()
