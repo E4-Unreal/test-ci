df12d0cebc37c22ee84da5fb5ba3d3a2c31ed6c7
이정훈 <eu4ng97@gmail.com>
2025-05-08 13:18:51 +0900

    Feat: 플러그인 파일명 변경 기능 추가

--- .github/workflows/scripts/modules/unreal_plugin.py
+++ .github/workflows/scripts/modules/unreal_plugin.py
@@ -15,14 +15,19 @@ def change_plugin_name(plugin_directory, old_plugin_name, new_plugin_name):
         sys.exit('Plugin name is same')
 
     # 플러그인 파일 확인
-    plugin_file_path = old_plugin_name + '.uplugin'
-    if not os.path.isfile(plugin_file_path):
-        sys.exit(plugin_file_path + ' is not exist')
+    plugin_extension = '.uplugin'
+    old_plugin_file_path = old_plugin_name + plugin_extension
+    if not os.path.isfile(old_plugin_file_path):
+        sys.exit(old_plugin_file_path + ' is not exist')
 
-    # 플러그인 이름 수정
-    with open(plugin_file_path, 'r+') as f:
+    # 플러그인 내용 수정
+    with open(old_plugin_file_path, 'r+') as f:
         lines = f.readlines()
         f.seek(0)
         for line in lines:
             line = line.replace(old_plugin_name, new_plugin_name)
             f.write(line)
+
+    # 플러그인 파일명 변경
+    new_plugin_file_path = new_plugin_name + plugin_extension
+    os.rename(old_plugin_file_path, new_plugin_file_path)
