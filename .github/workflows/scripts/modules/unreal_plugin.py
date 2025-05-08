d48be393195a6218200fce591baabec128b7f7a6
이정훈 <eu4ng97@gmail.com>
2025-05-08 22:42:14 +0900

    Feat: 모든 헤더 및 cpp 파일 내용 수정 기능 추가

--- .github/workflows/scripts/modules/unreal_plugin.py
+++ .github/workflows/scripts/modules/unreal_plugin.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import glob
 
 def replace_text(file_path: str, text_tuple: tuple[str, str]):
     with open(file_path, 'r', encoding='utf-8') as f:
@@ -22,16 +23,19 @@ def replace_texts(file_path: str, text_tuples: list[tuple[str, str]]):
 
 class UnrealPlugin:
     def __init__(self, plugin_directory = os.getcwd()):
+        self.plugin_directory = plugin_directory
         self.extension = '.uplugin'
         self.path = ''
         self.name = ''
         self.readme_path = 'README.md'
+        self.header_file_paths = []
+        self.cpp_file_paths = []
 
         # 작업 폴더 변경
-        if os.path.isdir(plugin_directory):
-            os.chdir(plugin_directory)
+        if os.path.isdir(self.plugin_directory):
+            os.chdir(self.plugin_directory)
         else:
-            sys.exit(plugin_directory + ' is not exist')
+            sys.exit(self.plugin_directory + ' is not exist')
 
         # 플러그인 파일명 불러오기
         file_list = os.listdir()
@@ -41,6 +45,12 @@ class UnrealPlugin:
             self.name = plugin_file_list[0].replace(self.extension, '')
         else:
             sys.exit('Plugin file is not found')
+
+        # 모든 헤더 파일 경로 불러오기
+        self.header_file_paths = glob.glob(os.path.join(self.plugin_directory, 'Source', self.name, '**', '*.h'), recursive=True)
+
+        # 모든 cpp 파일 경로 불러오기
+        self.cpp_file_paths = glob.glob(os.path.join(self.plugin_directory, 'Source', self.name, '**', '*.cpp'), recursive=True)
 
     def set_name(self, new_name):
         # 새로운 플러그인 이름 유효성 검사
@@ -59,3 +69,16 @@ class UnrealPlugin:
 
         # README 파일 수정
         replace_text(self.readme_path, plugin_name_tuple)
+
+        # 모든 헤더 파일 수정
+        text_tuples = [
+            plugin_name_tuple,
+            (self.name + '_API', new_name.upper() + '_API'),
+            (self.name.upper() + '_API', new_name.upper() + '_API')
+        ]
+        for header_file_path in self.header_file_paths:
+            replace_texts(header_file_path, text_tuples)
+
+        # 모든 cpp 파일 수정
+        for cpp_file_path in self.cpp_file_paths:
+            replace_text(cpp_file_path, plugin_name_tuple)
