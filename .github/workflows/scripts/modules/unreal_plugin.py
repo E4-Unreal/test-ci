7dcf6e4b9401dbbe29e2041999d320453ad54031
이정훈 <eu4ng97@gmail.com>
2025-05-08 23:27:22 +0900

    Feat: 모든 파일 내용 및 이름 수정 기능 추가

--- .github/workflows/scripts/modules/unreal_plugin.py
+++ .github/workflows/scripts/modules/unreal_plugin.py
@@ -25,11 +25,11 @@ class UnrealPlugin:
     def __init__(self, plugin_directory = os.getcwd()):
         self.plugin_directory = plugin_directory
         self.extension = '.uplugin'
-        self.path = ''
-        self.name = ''
-        self.readme_path = 'README.md'
+        self.path: str = None
+        self.name: str = None
+        self.readme_path = os.path.join(self.plugin_directory, 'README.md')
+        self.all_file_paths = []
         self.header_file_paths = []
-        self.cpp_file_paths = []
 
         # 작업 폴더 변경
         if os.path.isdir(self.plugin_directory):
@@ -37,20 +37,24 @@ class UnrealPlugin:
         else:
             sys.exit(self.plugin_directory + ' is not exist')
 
-        # 플러그인 파일명 불러오기
-        file_list = os.listdir()
-        plugin_file_list = [file for file in file_list if file.endswith(self.extension)]
-        if plugin_file_list:
-            self.path = plugin_file_list[0]
-            self.name = plugin_file_list[0].replace(self.extension, '')
+        # 플러그인 파일 경로 및 이름 불러오기
+        plugin_paths = glob.glob(os.path.join(self.plugin_directory, '*' + self.extension))
+        if plugin_paths:
+            self.path = plugin_paths[0]
+            self.name = os.path.basename(self.path).replace(self.extension, '')
         else:
             sys.exit('Plugin file is not found')
 
-        # 모든 헤더 파일 경로 불러오기
-        self.header_file_paths = glob.glob(os.path.join(self.plugin_directory, 'Source', self.name, '**', '*.h'), recursive=True)
+        # 모든 파일 경로 불러오기
+        all_file_paths = glob.glob(os.path.join(self.plugin_directory, 'Source', self.name, '**', '*.*'), recursive=True)
+        for file_path in all_file_paths:
+            if file_path.endswith('.h') or file_path.endswith('.cpp') or file_path.endswith('.cs'):
+                self.all_file_paths.append(file_path)
+        self.all_file_paths.append(self.path)
+        self.all_file_paths.append(self.readme_path)
 
-        # 모든 cpp 파일 경로 불러오기
-        self.cpp_file_paths = glob.glob(os.path.join(self.plugin_directory, 'Source', self.name, '**', '*.cpp'), recursive=True)
+        # 모든 헤더 파일 경로 불러오기
+        self.header_file_paths = [file_path for file_path in self.all_file_paths if file_path.endswith('.h')]
 
     def set_name(self, new_name):
         # 새로운 플러그인 이름 유효성 검사
@@ -59,26 +63,21 @@ class UnrealPlugin:
         elif self.name == new_name:
             sys.exit('Plugin name is same')
 
-        # 변수 선언
-        plugin_name_tuple = (self.name, new_name)
-        new_plugin_path = new_name + self.extension
-
-        # 플러그인 파일 수정
-        replace_text(self.path, plugin_name_tuple)
-        os.rename(self.path, new_plugin_path)
-
-        # README 파일 수정
-        replace_text(self.readme_path, plugin_name_tuple)
+        # 모든 파일 수정
+        for file_path in self.all_file_paths:
+            replace_text(file_path, (self.name, new_name))
 
         # 모든 헤더 파일 수정
         text_tuples = [
-            plugin_name_tuple,
             (self.name + '_API', new_name.upper() + '_API'),
             (self.name.upper() + '_API', new_name.upper() + '_API')
         ]
         for header_file_path in self.header_file_paths:
             replace_texts(header_file_path, text_tuples)
 
-        # 모든 cpp 파일 수정
-        for cpp_file_path in self.cpp_file_paths:
-            replace_text(cpp_file_path, plugin_name_tuple)
+        # 모든 파일명 수정
+        for file_path in self.all_file_paths:
+            dirname = os.path.dirname(file_path)
+            basename = os.path.basename(file_path)
+            if self.name in basename:
+                os.rename(file_path, os.path.join(dirname, basename.replace(self.name, new_name)))
