d3e7a94c00cb70f40f81d16396efbfcb351015d6
이정훈 <eu4ng97@gmail.com>
2025-05-08 13:27:49 +0900

    Feat: replace_text 함수 작성 및 README 업데이트 기능 추가

--- .github/workflows/scripts/modules/unreal_plugin.py
+++ .github/workflows/scripts/modules/unreal_plugin.py
@@ -1,6 +1,14 @@
 import os
 import sys
 
+def replace_text(file_path, old_text, new_text):
+    with open(file_path, 'r+') as f:
+        lines = f.readlines()
+        f.seek(0)
+        for line in lines:
+            line = line.replace(old_text, new_text)
+            f.write(line)
+
 def change_plugin_name(plugin_directory, old_plugin_name, new_plugin_name):
     # 작업 폴더 변경
     if os.path.isdir(plugin_directory):
@@ -17,17 +25,14 @@ def change_plugin_name(plugin_directory, old_plugin_name, new_plugin_name):
     # 플러그인 파일 확인
     plugin_extension = '.uplugin'
     old_plugin_file_path = old_plugin_name + plugin_extension
+    new_plugin_file_path = new_plugin_name + plugin_extension
     if not os.path.isfile(old_plugin_file_path):
         sys.exit(old_plugin_file_path + ' is not exist')
 
-    # 플러그인 내용 수정
-    with open(old_plugin_file_path, 'r+') as f:
-        lines = f.readlines()
-        f.seek(0)
-        for line in lines:
-            line = line.replace(old_plugin_name, new_plugin_name)
-            f.write(line)
-
-    # 플러그인 파일명 변경
-    new_plugin_file_path = new_plugin_name + plugin_extension
+    # 플러그인 이름 수정
+    replace_text(old_plugin_file_path, old_plugin_name, new_plugin_name)
     os.rename(old_plugin_file_path, new_plugin_file_path)
+
+    # README 수정
+    readme_file_path = 'README.md'
+    replace_text(readme_file_path, old_plugin_name, new_plugin_name)
