4b22b7d81f9817dd0c7d088edf249e659682eda3
이정훈 <eu4ng97@gmail.com>
2025-05-08 14:11:48 +0900

    Refactor: unreal_plugin

--- .github/workflows/scripts/change_plugin_name.py
+++ .github/workflows/scripts/change_plugin_name.py
@@ -4,4 +4,4 @@ from modules.unreal_plugin import UnrealPlugin
 if __name__ == '__main__':
     new_plugin_name = os.environ['NEW_PLUGIN_NAME']
     unreal_plugin = UnrealPlugin()
-    unreal_plugin.set_plugin_name(new_plugin_name)
+    unreal_plugin.set_name(new_plugin_name)
--- .github/workflows/scripts/modules/unreal_plugin.py
+++ .github/workflows/scripts/modules/unreal_plugin.py
@@ -12,8 +12,8 @@ def replace_text(file_path, old_text, new_text):
 class UnrealPlugin:
     def __init__(self, plugin_directory = os.getcwd()):
         self.extension = '.uplugin'
-        self.plugin_path = ''
-        self.plugin_name = ''
+        self.path = ''
+        self.name = ''
         self.readme_path = 'README.md'
 
         # 작업 폴더 변경
@@ -26,22 +26,22 @@ class UnrealPlugin:
         file_list = os.listdir()
         plugin_file_list = [file for file in file_list if file.endswith(self.extension)]
         if plugin_file_list:
-            self.plugin_path = plugin_file_list[0]
-            self.plugin_name = plugin_file_list[0].replace(self.extension, '')
+            self.path = plugin_file_list[0]
+            self.name = plugin_file_list[0].replace(self.extension, '')
         else:
             sys.exit('Plugin file is not found')
 
-    def set_plugin_name(self, new_plugin_name):
+    def set_name(self, new_name):
         # 새로운 플러그인 이름 유효성 검사
-        if new_plugin_name is None:
+        if new_name is None:
             sys.exit('Plugin name is None')
-        elif self.plugin_name == new_plugin_name:
+        elif self.name == new_name:
             sys.exit('Plugin name is same')
 
         # 플러그인 파일 수정
-        replace_text(self.plugin_path, self.plugin_name, new_plugin_name)
-        new_plugin_path = new_plugin_name + self.extension
-        os.rename(self.plugin_path, new_plugin_path)
+        replace_text(self.path, self.name, new_name)
+        new_plugin_path = new_name + self.extension
+        os.rename(self.path, new_plugin_path)
 
         # README 파일 수정
-        replace_text(self.readme_path, self.plugin_name, new_plugin_name)
+        replace_text(self.readme_path, self.name, new_name)
