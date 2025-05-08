994d7f68a93ab5d1e8ff2944529522fef6cb592e
이정훈 <eu4ng97@gmail.com>
2025-05-08 22:07:21 +0900

    Chore: PLUGIN_NAME 모듈 추가

---
+++ Source/PLUGIN_NAME/Private/PLUGIN_NAME.cpp
@@ -0,0 +1,17 @@
+﻿#include "PLUGIN_NAME.h"
+
+#define LOCTEXT_NAMESPACE "FPLUGIN_NAMEModule"
+
+void FPLUGIN_NAMEModule::StartupModule()
+{
+    
+}
+
+void FPLUGIN_NAMEModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FPLUGIN_NAMEModule, PLUGIN_NAME)
