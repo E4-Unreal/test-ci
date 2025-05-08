f627244e850f9c6416e35fe1429c9e817c5e331f
이정훈 <eu4ng97@gmail.com>
2025-05-08 15:09:13 +0900

    Fix: 폴더 구조 변경

--- .github/workflows/scripts/change_plugin_name/main.py
+++ .github/workflows/scripts/change_plugin_name/main.py
@@ -1,4 +1,7 @@
 import os
+import sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from modules.unreal_plugin import UnrealPlugin
 
 if __name__ == '__main__':
